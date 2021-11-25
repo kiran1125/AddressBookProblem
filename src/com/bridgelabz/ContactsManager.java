fa164e837e57cdb808256442807c91bbf103ed26
Kiran <makamkiran95@gmail.com>
2021-11-25 08:20:06 +0530

    Added : Editing the existing contact by using the name

--- src/com/bridgelabz/ContactsManager.java
+++ src/com/bridgelabz/ContactsManager.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
  */
 public class ContactsManager {
     //Attributes
-    Scanner sc;
+    Scanner sc = new Scanner(System.in);
+    Contact contact;
     /**
      * this method is to add the contact
      */
-    public void add(){
-        sc= new Scanner(System.in);
+    public void addingContact(){
         //print the statement
         System.out.println("Enter the name");
         //assigning the name given by user to the name
@@ -37,6 +37,74 @@ public class ContactsManager {
         //defining the zipcode given by user
         long zipCode = sc.nextLong();
         //creating a contact
-        Contact contact = new Contact(name, address, city, state, email, phoneNumber, zipCode);
+        contact = new Contact(name, address, city, state, email, phoneNumber, zipCode);
+    }
+
+    /**
+     * this method is to edit the existing contact
+     */
+    public void editingContact() {
+        //using the scanner class taking the input from user to edit contact
+        System.out.println("Enter the Name of contact to Edit");
+        //taking the name from the user
+        String name1 = sc.next();
+        //checking the condition if the name which was added contact
+        if (contact.name.equals (name1)) {
+            //printing the statements that user want to choose
+            System.out.println("\n1 : Change the name");
+            System.out.println("2 : Change the address");
+            System.out.println("3 : Change the city");
+            System.out.println("4 : Change the state");
+            System.out.println("5 : Change the emailId");
+            System.out.println("6 : Change the Phone number");
+            System.out.println("7 : Change the zipcode");
+            System.out.println("0 : Exit");
+            //taking user input as selection and checking that with switch case
+            int selection = sc.nextInt();
+            switch (selection) {
+                case 1: {
+                    System.out.println("Enter new Name");
+                    contact.name = sc.next();       //changes the name to user defined name
+                    break;
+                }
+                case 2: {
+                    System.out.println("Enter new address");
+                    contact.address = sc.next();           //changes the address by user defined address
+                    break;
+                }
+                case 3: {
+                    System.out.println("Enter the new city");
+                    contact.city = sc.next();
+                    break;
+                }
+                case 4: {
+                    System.out.println("Enter the new state");
+                    contact.state = sc.next();         //Changes the state
+                    break;
+                }
+                case 5: {
+                    System.out.println("Enter the new email");
+                    contact.email = sc.next();        //Changes the email
+                    break;
+                }
+                case 6: {
+                    System.out.println("Enter the new phoneNumber");
+                    contact.phoneNumber = sc.nextLong();     //changes the phone number
+                    break;
+                }
+                case 7: {
+                    System.out.println("Enter the new Zipcode");
+                    contact.zipCode = sc.nextLong();         //changes the zipCode
+                    break;
+                }
+                default:
+                    break;
+            }
+            System.out.println("After Editing the contact");
+            //  To print the contact after Editing
+            System.out.println(contact.name + " " +contact.state + " " + contact.address + " " +contact.city + " " + contact.email + " " +contact.phoneNumber + " " + contact.zipCode);
+        }
+
+
     }
 }
